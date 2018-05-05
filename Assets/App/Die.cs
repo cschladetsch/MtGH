53307aba19a661712f5ed82f9057fc2325b9b0a4
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-05-05 23:18:04 +1000

    Add die cage

---
+++ Assets/App/Die.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die :  MonoBehaviour
+{
+
+    public Vector3 InitForce;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var rb = GetComponent<Rigidbody>();
+        rb.AddForce(InitForce.x, InitForce.y, InitForce.z, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
