0d0419efbc2e877129e6b24776d68933bd547135
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-05-06 01:47:51 +1000

    Die roll physics over UI

---
+++ Assets/App/DiceCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class DiceCanvas : MonoBehaviour
+{
+    public Main Main;
+
+    public void BackgroundPressed()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Reset()
+    {
+    }
+}
--- Assets/App/Die.cs
+++ Assets/App/Die.cs
@@ -1,21 +1,137 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Die :  MonoBehaviour
 {
+    public int UnitVel = 1000;
 
-    public Vector3 InitForce;
-
-	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-	    var rb = GetComponent<Rigidbody>();
-        rb.AddForce(InitForce.x, InitForce.y, InitForce.z, ForceMode.Impulse);
+	    _rb = GetComponent<Rigidbody>();
+
+        startHeight = transform.position.z;
+	    startPos = transform.position;
+
+	    Reset();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private Vector3 startPos;
+
+    void Reset()
+    {
+	    _rb.isKinematic = true;
+        _rb.freezeRotation = false;
+        _rb.velocity = Vector3.zero;
+
+        transform.position = startPos;
+	    transform.rotation = Random.rotationUniform;
+
+	    rolling = false;
+	    stopped = false;
+        letGo = false;
+
+        NewRotationSeek();
+    }
+
+    void NewRotationSeek()
+    {
+        rotationSeek = Random.rotationUniform;
+        rotationSeekRate = Random.Range(1.5f, 3.5f);
+        rotationSeekTime = Random.Range(1, 2);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Reset();
+            return;
+        }
+
+        // when we first let go, we apply a force, but it can take a frame to make it affect velocities
+        if (letGo)
+        {
+            letGo = false;
+            return;
+        }
+
+        if (stopped)
+            return;
+
+        if (!rolling)
+        {
+            rotationSeekTime -= Time.deltaTime;
+            if (rotationSeekTime < 0)
+                NewRotationSeek();
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationSeek, rotationSeekRate*Time.deltaTime);
+            return;
+        }
+
+        var v = _rb.velocity.magnitude;
+        var r = _rb.angularVelocity.magnitude;
+
+        if (v < 0.01f && r < 0.01f)
+        {
+            Debug.Log("Stopped roll");
+            rolling = false;
+            stopped = true;
+            _rb.velocity = Vector3.zero;
+            _rb.freezeRotation = true;
+            _rb.isKinematic = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (rolling || stopped)
+            return;
+
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        lastScreenPoint = screenPoint;
+        screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        velocity = screenPoint - lastScreenPoint;
+        Vector3 world = Camera.main.ScreenToWorldPoint(screenPoint) + offset;
+        transform.position = world;
+    }
+
+    void OnMouseUp()
+    {
+        var unitVel = UnitVel;
+        var force = unitVel * velocity;
+        force.z *= -1.5f;
+        Debug.Log("Initial force: " + force);
+        force.x = Mathf.Min(100000.0f, force.x);
+        force.y = Mathf.Min(100000.0f, force.y);
+        force.z = Mathf.Min(100000.0f, force.z);
+        //_rb.AddForce(force.x, force.y, force.z, ForceMode.
+        //_rb.AddRelativeForce(force);
+	    _rb.isKinematic = false;
+        _rb.AddForce(force, ForceMode.Force);
+        rolling = true;
+        letGo = true;
+    }
+
+    private Rigidbody _rb;
+    private float startHeight;
+
+    private Vector3 offset;
+
+    private Vector3 screenPoint;
+    private Vector3 lastScreenPoint;
+    private Vector3 velocity;
+
+    private float rotationSeekTime;
+    private float rotationSeekRate;
+    private Quaternion rotationSeek;
+
+    private bool rolling = false;
+    private bool stopped = false;
+
+    private bool letGo = false;
+
 }
